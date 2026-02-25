dad44acaf820ad2b91df120ec1b6148331d2aee5
Rui Xue <ruixue.fi@gmail.com>
2026-02-25 09:27:43 +0200

    #9 restructuring refore actual change

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from 'react-router'
 import CV from './components/CV'
-import cv from './assets/cv.json'
+import data from './assets/cv.json'
+import metadata from './assets/cv_metadata.json'
 import './App.css'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<CV json={cv} mode="html" />} />
-      <Route path="/pdf" element={<CV json={cv} mode="pdf" />} />
+      <Route path="/" element={<CV data={data} metadata={metadata} mode="html" />} />
+      <Route path="/pdf" element={<CV data={data} metadata={metadata} mode="pdf" />} />
     </Routes>
   )
 }
